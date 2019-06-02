05e382726919fba63d29973a25d449be1b537fc7
Parker <parker.smith725@gmail.com>
2019-06-01 20:02:05 -0600

    update

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import './LandingPage.css';
 
 class LandingPage extends React.Component{
     render(){
         return (
             <div>
-                Landing Page
+                <div className="hero-section">
+                    <div className="container-fluid hero-container"> 
+                        <div className="row hero-row align-items-center">
+                            <div className="col-12 hero-col">
+                                <h1 className="hero-header">
+                                    Some cool statement here
+                                </h1>
+                                <Link to='/events'>
+                                    <div className="hero-button">
+                                        Get Started
+                                    </div>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
 import Router from './router/Router';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 
 const app = (
